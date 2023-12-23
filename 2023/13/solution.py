20355ace711fc7af504fc0d3b48daeeb0d12b3c0
Willem Mouwen <willem@mouwen.dev>
2023-12-23 16:56:34 +0100

    2023 day 13 part 1

--- 2023/13/solution.py
+++ 2023/13/solution.py
@@ -3,21 +3,35 @@ import sys
 Pattern = list[str]
 
 
-def solve(pattern: Pattern) -> int:
-    # TODO
-    print(pattern)
-    return 5
+def horizontal_reflections(pattern: Pattern) -> int:
+    score = 0
+
+    for pivot in range(len(pattern) - 1):
+        reflection = True
+        offset = 0
+
+        while pivot - offset >= 0 and pivot + 1 + offset < len(pattern):
+            if pattern[pivot - offset] != pattern[pivot + 1 + offset]:
+                reflection = False
+                break
+            offset += 1
+
+        if reflection:
+            score += pivot + 1
+
+    return score
 
 
 points = 0
 pattern: Pattern = []
 for line in map(lambda line: line.strip(), sys.stdin):
     if not line:
-        points += solve(pattern)
+
+        points += 100 * horizontal_reflections(pattern) + horizontal_reflections(list(zip(*pattern)))
         pattern = []
     else:
         pattern.append(line)
 
-points += solve(pattern)
+points += 100 * horizontal_reflections(pattern) + horizontal_reflections(list(zip(*pattern)))
 
 print(points)
