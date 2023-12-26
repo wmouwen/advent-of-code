3c94159f3c242b80cfd779e6b10daa5e3d356715
Willem Mouwen <willem@mouwen.dev>
2023-12-26 19:45:33 +0100

    Finish inefficient algorithm for 2023 day 5

--- 2023/5/solution.py
+++ 2023/5/solution.py
@@ -4,30 +4,39 @@ import sys
 seeds = list(map(int, re.findall(r'\d+', sys.stdin.readline())))
 mappings = []
 
-# source = None
-# destination = None
 for line in sys.stdin:
     if line.strip() == '':
         continue
 
     match = re.match(r'^(?P<source>\w+)-to-(?P<destination>\w+) map:$', line)
     if match is not None:
-        # source = match['source']
-        # destination = match['destination']
         mappings.append([])
         continue
 
     mappings[-1].append(re.match(r'^(?P<destination>\d+) (?P<start>\d+) (?P<length>\d+)', line))
 
-locations = []
-for i in range(0, len(seeds)):
-    location = seeds[i]
+best = sys.maxsize
+for location in seeds:
     for mapping in mappings:
         for match in mapping:
             if int(match['start']) <= location < int(match['start']) + int(match['length']):
                 location += int(match['destination']) - int(match['start'])
                 break
 
-    locations.append(location)
+    best = min(best, location)
 
-print(min(locations))
+print(best)
+
+# TODO Improve performance
+best = sys.maxsize
+for s in range(0, len(seeds), 2):
+    for location in range(seeds[s], seeds[s] + seeds[s + 1]):
+        for mapping in mappings:
+            for match in mapping:
+                if int(match['start']) <= location < int(match['start']) + int(match['length']):
+                    location += int(match['destination']) - int(match['start'])
+                    break
+
+        best = min(best, location)
+
+print(best)
