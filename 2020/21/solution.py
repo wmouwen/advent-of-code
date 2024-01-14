be7da13ced886f7a4fedfcb40b093627e605e67d
Willem Mouwen <willem@mouwen.dev>
2024-01-14 13:47:54 +0100

    2020 day 21 grooming

--- 2020/21/solution.py
+++ 2020/21/solution.py
@@ -10,20 +10,19 @@ class Food:
 
 def main():
     foods: list[Food] = []
-    for line in sys.stdin:
-        match = re.match(r'([\w\s]+) \(contains ([\w\s,]+)\)', line.strip())
-
-        foods.append(Food(
-            ingredients=set(match.group(1).split(' ')),
-            allergens=set(match.group(2).split(', '))
-        ))
-
     ingredients: set = set()
     allergens: dict = {}
 
-    for food in foods:
-        ingredients |= food.ingredients
+    for line in sys.stdin:
+        match = re.match(r'([\w\s]+) \(contains ([\w\s,]+)\)', line.strip())
 
+        food = Food(
+            ingredients=set(match.group(1).split(' ')),
+            allergens=set(match.group(2).split(', '))
+        )
+        foods.append(food)
+
+        ingredients |= food.ingredients
         for allergen in food.allergens:
             if allergen in allergens:
                 allergens[allergen] &= food.ingredients
@@ -37,11 +36,11 @@ def main():
     print(sum(len(allergen_free & food.ingredients) for food in foods))
 
     allergen_known = set()
-    for _ in range(100):
-        for allergen in sorted(allergens, key=lambda a: len(allergens[a]), reverse=True):
+    while any(len(candidates) > 1 for candidates in allergens.values()):
+        for allergen in sorted(allergens, key=lambda a: len(allergens[a])):
             if len(allergens[allergen]) > 1:
                 allergens[allergen] -= allergen_known
-
+            assert len(allergens[allergen]) > 0
             if len(allergens[allergen]) == 1:
                 allergen_known |= allergens[allergen]
 
