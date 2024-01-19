33d3a2ebb5ffb92b10bc265a933f1fedc45b59bb
Willem Mouwen <willem@mouwen.dev>
2024-01-19 12:58:48 +0100

    2016 day 22 part 2, manual with assumptions

--- 2016/22/solution.py
+++ 2016/22/solution.py
@@ -1,3 +1,4 @@
+import itertools
 import re
 import sys
 
@@ -12,13 +13,34 @@ class Node:
 
 
 def main():
-    nodes = []
+    nodes = {}
 
     for line in sys.stdin:
         if match := re.match(r'/dev/grid/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+\d+%', line.strip()):
-            nodes.append(Node(*map(int, match.groups())))
+            node = Node(*map(int, match.groups()))
+            if node.y not in nodes:
+                nodes[node.y] = {}
+            nodes[node.y][node.x] = node
 
-    print(sum(a != b and 0 < a.used <= b.available for a in nodes for b in nodes))
+    all_nodes = set(itertools.chain.from_iterable(map(lambda row: row.values(), nodes.values())))
+    print(sum(a != b and 0 < a.used <= b.available for a in all_nodes for b in all_nodes))
+
+    # No node can contain data of two disks combined
+    assert all(a.used + b.used > a.size and a.used + b.used > b.size
+               for a in all_nodes if a.used > 0
+               for b in all_nodes if b.used > 0 and a != b)
+
+    # There is exactly one empty slot
+    assert sum(node.used == 0 for node in all_nodes) == 1
+
+    # Required data fits on target disk
+    assert nodes[0][len(nodes[0]) - 1].used <= nodes[0][0].size
+
+    # Manual solution for specific input:
+    # slot: 7 left, 14 up, 13 right
+    # data: 34 steps left
+    # slot: 33 circle movements, 4 steps each, around data
+    print(7 + 14 + 14 + 34 + 4 * 33)
 
 
 if __name__ == '__main__':
