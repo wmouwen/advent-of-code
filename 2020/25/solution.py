14cbb86106a78a3df8ddf018bbb3509dcecaf4e1
Willem Mouwen <willem@mouwen.dev>
2024-03-24 18:17:49 +0100

    2020 day 25

---
+++ 2020/25/solution.py
@@ -0,0 +1,33 @@
+import sys
+
+
+def transform(subject_number: int, loop_size: int):
+    output = 1
+    for _ in range(loop_size):
+        output = (output * subject_number) % 20201227
+
+    return output
+
+
+def find_loop_size(public_key: int):
+    output = 1
+    loop_size = 1
+
+    while True:
+        output = (output * 7) % 20201227
+
+        if output == public_key:
+            return loop_size
+
+        loop_size += 1
+
+
+def main():
+    print(transform(
+        int(sys.stdin.readline()),
+        find_loop_size(int(sys.stdin.readline()))
+    ))
+
+
+if __name__ == "__main__":
+    main()
