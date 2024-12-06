2b0eda85ebc5907580956f6ffe7de9ef73adcc08
Willem Mouwen <willem@mouwen.dev>
2024-12-06 14:43:50 +0100

    2024 day 6 part 2

--- 2024/6/solution.py
+++ 2024/6/solution.py
@@ -6,32 +6,17 @@ from math import floor
 Vector = namedtuple('Vector', 'y x')
 
 
-def find_guard_start(grid) -> Vector:
-    for y, row in enumerate(grid):
-        for x, cell in enumerate(row):
-            if cell == '^':
-                return Vector(y=y, x=x)
-
-    raise Exception('No guard found')
+class LoopException(Exception):
+    pass
 
 
-def main():
-    grid: list[list[str]] = []
+def steps_to_exit(grid: list[list[str]]) -> set[Vector]:
     dirs = [Vector(y=-1, x=0), Vector(y=0, x=1), Vector(y=1, x=0), Vector(y=0, x=-1)]
-
-    for line in sys.stdin:
-        if line.strip() == '':
-            break
-
-        grid.append(list(line.strip()))
-
     guard_start = find_guard_start(grid)
-
     curr_pos = guard_start
     curr_dir = 0
     visited_positions = {curr_pos}
     patrol_route = {(curr_pos, curr_dir)}
-    # obstacle_candidates = {}
 
     while True:
         next_pos = Vector(y=curr_pos.y + dirs[curr_dir].y, x=curr_pos.x + dirs[curr_dir].x)
@@ -45,13 +30,48 @@ def main():
             break
 
         curr_pos = next_pos
-        visited_positions.add(curr_pos)
+
+        if (curr_pos, curr_dir) in patrol_route:
+            raise LoopException
+
         patrol_route.add((curr_pos, curr_dir))
+        visited_positions.add(curr_pos)
 
-        # if (curr_pos, (curr_dir + 1) % 4) in patrol_route:
-        #     print('obstacle?')
+    return visited_positions
 
+
+def find_guard_start(grid) -> Vector:
+    for y, row in enumerate(grid):
+        for x, cell in enumerate(row):
+            if cell == '^':
+                return Vector(y=y, x=x)
+
+    raise Exception('No guard found')
+
+
+def main():
+    grid: list[list[str]] = []
+
+    for line in sys.stdin:
+        if line.strip() == '':
+            break
+
+        grid.append(list(line.strip()))
+
+    visited_positions = steps_to_exit(grid)
     print(len(visited_positions))
+
+    visited_positions.remove(find_guard_start(grid))
+    obstacles_causing_loops = 0
+    for position in visited_positions:
+        grid[position.y][position.x] = '#'
+        try:
+            steps_to_exit(grid)
+        except LoopException:
+            obstacles_causing_loops += 1
+        grid[position.y][position.x] = '.'
+
+    print(obstacles_causing_loops)
 
 
 if __name__ == '__main__':
