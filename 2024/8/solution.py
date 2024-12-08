5542a192fa28b2a7f24def247d2123a96241744d
Willem Mouwen <willem@mouwen.dev>
2024-12-08 09:26:13 +0100

    2024 day 8 part 2

--- 2024/8/solution.py
+++ 2024/8/solution.py
@@ -18,17 +18,21 @@ def main():
                 antennas[cell] = set()
             antennas[cell].add(Vector(y=y, x=x))
 
-    antinodes: set[Vector] = set()
+    antinodes_close: set[Vector] = set()
+    antinodes_all: set[Vector] = set()
 
     for antenna_set in antennas.values():
         for combination in combinations(antenna_set, 2):
             y_offset, x_offset = combination[0].y - combination[1].y, combination[0].x - combination[1].x
-            antinodes.add(Vector(y=combination[0].y + y_offset, x=combination[0].x + x_offset))
-            antinodes.add(Vector(y=combination[1].y - y_offset, x=combination[1].x - x_offset))
-            # print(combination)
 
-    print(len(list(filter(lambda antinode: 0 <= antinode.x <= x_max and 0 <= antinode.y <= y_max, antinodes))))
-    # print(*[''.join(row) for row in grid], sep="\n")
+            antinodes_close.add(Vector(y=combination[0].y + y_offset, x=combination[0].x + x_offset))
+            antinodes_close.add(Vector(y=combination[1].y - y_offset, x=combination[1].x - x_offset))
+
+            for k in range(-1 * max(y_max, x_max), max(y_max, x_max)):
+                antinodes_all.add(Vector(y=combination[0].y + y_offset * k, x=combination[0].x + x_offset * k))
+
+    print(len(list(filter(lambda antinode: 0 <= antinode.x <= x_max and 0 <= antinode.y <= y_max, antinodes_close))))
+    print(len(list(filter(lambda antinode: 0 <= antinode.x <= x_max and 0 <= antinode.y <= y_max, antinodes_all))))
 
 
 if __name__ == '__main__':
