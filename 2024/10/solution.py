d22364c944ade08ba95b3b70e8551fe68108304e
Willem Mouwen <willem@mouwen.dev>
2024-12-10 19:36:26 +0100

    2024 day 10

---
+++ 2024/10/solution.py
@@ -0,0 +1,36 @@
+import sys
+
+
+def find_trails(grid: list[list[int]], x: int, y: int, path: list[tuple[int, int]] = []):
+    if grid[y][x] == 9:
+        return [path + [(x, y)]]
+
+    return [
+        trail
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        if 0 <= y + dy < len(grid) and 0 <= x + dx < len(grid[y]) and grid[y + dy][x + dx] == grid[y][x] + 1
+        for trail in find_trails(grid, x=x + dx, y=y + dy, path=path + [(x, y)])
+    ]
+
+
+def main():
+    grid = [
+        list(map(int, line.strip()))
+        for line in sys.stdin
+        if line.strip()
+    ]
+
+    trails = [
+        trail
+        for y in range(len(grid))
+        for x in range(len(grid[y]))
+        if grid[y][x] == 0
+        for trail in find_trails(grid, x=x, y=y)
+    ]
+
+    print(len(set((trail[0], trail[-1]) for trail in trails)))
+    print(len(trails))
+
+
+if __name__ == '__main__':
+    main()
