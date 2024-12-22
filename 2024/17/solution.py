aa8108abc22d3efd62e445b9e1cad6fa53132d5e
Willem Mouwen <willem@mouwen.dev>
2024-12-22 12:52:33 +0100

    2024 day 17 part 2

--- 2024/17/solution.py
+++ 2024/17/solution.py
@@ -1,6 +1,5 @@
 import re
 import sys
-from math import inf
 
 
 def combo(operand, registers):
@@ -17,16 +16,50 @@ def run(registers, instructions):
 
         ip += 2
 
-        if opcode == 0: registers[0] = registers[0] // pow(2, combo_operand)
-        if opcode == 1: registers[1] = registers[1] ^ literal_operand
-        if opcode == 2: registers[1] = combo_operand % 8
+        if opcode == 0: registers[0] >>= combo_operand
+        if opcode == 1: registers[1] ^= literal_operand
+        if opcode == 2: registers[1] = combo_operand & 7
         if opcode == 3 and registers[0] > 0: ip = literal_operand
-        if opcode == 4: registers[1] = registers[1] ^ registers[2]
-        if opcode == 5: output.append(combo_operand % 8)
-        if opcode == 6: registers[1] = registers[0] // pow(2, combo_operand)
-        if opcode == 7: registers[2] = registers[0] // pow(2, combo_operand)
+        if opcode == 4: registers[1] ^= registers[2]
+        if opcode == 5: output.append(combo_operand & 7)
+        if opcode == 6: registers[1] = registers[0] >> combo_operand
+        if opcode == 7: registers[2] = registers[0] >> combo_operand
 
     return output
+
+
+# Observations:
+# - Puzzle input is handcrafted with a purpose in mind and can be reverse engineered into the below program.
+#       while a > 0:
+#         b = (a & 7) ^ 7
+#         b = b ^ (a >> b) ^ 7
+#         output.append(b & 7)
+#         a = a >> 3
+# - Puzzle input of registers B and C doesn't matter.
+# - The value of register A is processed three bits at a time; each triplet adds one number to the output.
+# - The least significant triplet of bits only influences the first number of the output.
+# - The most significant triplet of bits solely determines the last number of the output.
+# Given the observations, a DFS on bit triplets can be performed.
+def search(instructions: list[int], attempt: list[int] = None, index: int = 0):
+    if attempt is None:
+        attempt = [0] * len(instructions)
+
+    for i in range(8):
+        attempt[index] = i
+        reg_a = sum(val << 3 * (len(attempt) - i - 1) for i, val in enumerate(attempt))
+        output = run([reg_a, 0, 0], instructions)
+
+        if len(output) != len(instructions):
+            continue
+
+        if output[-(index + 1)] == instructions[-(index + 1)]:
+            if index + 1 == len(instructions):
+                return reg_a
+
+            if (sub := search(instructions, attempt.copy(), index + 1)) is not None:
+                return sub
+
+    return None
 
 
 def main():
@@ -37,14 +70,7 @@ def main():
     output = run(registers.copy(), instructions)
     print(','.join(map(str, output)))
 
-    for i in range(pow(2, 100)):
-        registers[0] = i
-
-        output = run(registers.copy(), instructions)
-
-        if output == instructions:
-            print(i)
-            break
+    print(search(instructions))
 
 
 if __name__ == '__main__':
