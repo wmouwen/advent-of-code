79b21c0e04e1271920500aad6e16d92a8da0a681
Willem Mouwen <willem@mouwen.dev>
2024-12-22 14:24:06 +0100

    2024 day 16 part 2

--- 2024/16/solution.py
+++ 2024/16/solution.py
@@ -11,6 +11,38 @@ class Cell:
         self.neighbors = dict()
 
 
+def find_shortest_paths(cells, start, end):
+    distances = {}
+    shortest_paths = {}
+
+    queue = PriorityQueue()
+    queue.put((0, start.x, start.y, 'E', []))
+
+    while not queue.empty():
+        distance, x, y, direction, path = queue.get()
+        cell = cells[x, y]
+
+        if (cell, direction) in distances and distance > distances[cell, direction]:
+            continue
+
+        if (cell, direction) in distances and distance == distances[cell, direction]:
+            shortest_paths[(cell, direction)].append(path)
+
+        if (cell, direction) not in distances or distance < distances[cell, direction]:
+            shortest_paths[(cell, direction)] = [path]
+            distances[(cell, direction)] = distance
+
+        if direction in cell.neighbors:
+            neighbor = cell.neighbors[direction]
+            queue.put((distance + 1, neighbor.x, neighbor.y, direction, path + [(x, y, direction)]))
+
+        for turn in [-1, +1]:
+            new_direction = dirs[(dirs.index(direction) + turn) % len(dirs)]
+            queue.put((distance + 1000, cell.x, cell.y, new_direction, path + [(x, y, direction)]))
+
+    return shortest_paths, distances
+
+
 def main():
     grid = [list(line.strip()) for line in sys.stdin]
 
@@ -18,10 +50,11 @@ def main():
     start, end = None, None
     for y, row in enumerate(grid):
         for x, cell in enumerate(row):
-            if cell == '#':
-                continue
+            if cell == '#': continue
 
             cells[x, y] = Cell(x, y)
+            if cell == 'S': start = cells[x, y]
+            if cell == 'E': end = cells[x, y]
 
             if (x - 1, y) in cells:
                 cells[x - 1, y].neighbors['E'] = cells[x, y]
@@ -31,32 +64,22 @@ def main():
                 cells[x, y - 1].neighbors['S'] = cells[x, y]
                 cells[x, y].neighbors['N'] = cells[x, y - 1]
 
-            if cell == 'S': start = cells[x, y]
-            if cell == 'E': end = cells[x, y]
-
     assert start is not None and end is not None
 
-    shortest_paths = {}
-    previous_path = {}
-    queue = PriorityQueue()
-    queue.put((0, start.x, start.y, 'E', None, None, None))
-    while not queue.empty():
-        distance, x, y, direction, px, py, pd = queue.get()
-        cell = cells[x, y]
+    shortest_paths, distances = find_shortest_paths(cells, start, end)
 
-        if (cell, direction) in shortest_paths and distance >= shortest_paths[cell, direction]: continue
-        shortest_paths[(cell, direction)] = distance
-        previous_path[(cell, direction)] = (px, py, pd)
+    min_distance = min(distance for (cell, _), distance in distances.items() if cell == end)
+    print(min_distance)
 
-        if direction in cell.neighbors:
-            neighbor = cell.neighbors[direction]
-            queue.put((distance + 1, neighbor.x, neighbor.y, direction, cell.x, cell.y, direction))
+    fields_on_optimal_path = {(start.x, start.y), (end.x, end.y)}
+    for direction in dirs:
+        if (end, direction) not in shortest_paths or distances[(end, direction)] > min_distance:
+            continue
 
-        for turn in [-1, +1]:
-            new_direction = dirs[(dirs.index(direction) + turn) % len(dirs)]
-            queue.put((distance + 1000, cell.x, cell.y, new_direction, cell.x, cell.y, direction))
+        for path in shortest_paths[(end, direction)]:
+            fields_on_optimal_path = fields_on_optimal_path.union(set((x, y) for (x, y, _) in path))
 
-    print(min(distance for (cell, _), distance in shortest_paths.items() if cell == end))
+    print(len(fields_on_optimal_path))
 
 
 if __name__ == '__main__':
