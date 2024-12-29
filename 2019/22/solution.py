f9d26b3b4237b5c622f0e1a4bdc3b9e4b231bee0
Willem Mouwen <willem@mouwen.dev>
2024-12-29 16:32:43 +0100

    2019 day 22 part 2

--- 2019/22/solution.py
+++ 2019/22/solution.py
@@ -2,63 +2,51 @@ import re
 import sys
 
 
-def simulate(moves, size):
-    deck = list(range(size))
-
-    for move in moves:
-        if move == 'deal into new stack':
-            deck.reverse()
-
-        elif match := re.match(r'cut (-?\d+)', move):
-            cut = int(match.group(1))
-            deck = deck[cut:] + deck[:cut]
-
-        elif match := re.match(r'deal with increment (\d+)', move):
-            increment = int(match.group(1))
-
-            deck, old = [0 for _ in range(len(deck))], deck
-            for i in range(len(deck)):
-                deck[(increment * i) % len(deck)] = old[i]
-
-    return deck
-
-
-def shuffle(moves: list[str], size: int):
+def get_coefficients(moves: list[str], deck_size: int):
     a, b = 1, 0
 
     for move in moves[::-1]:
-        if move == 'deal into new stack':
-            a = -1 * a
-            b = size - b - 1
-
-        elif match := re.match(r'cut (-?\d+)', move):
+        if match := re.match(r'cut (-?\d+)', move):
             cut = int(match.group(1))
-            b = (b + cut) % size
+            b = (b + cut) % deck_size
+
+        elif move == 'deal into new stack':
+            a, b = -1 * a, deck_size - b - 1
 
         elif match := re.match(r'deal with increment (\d+)', move):
             increment = int(match.group(1))
-            modinv = pow(increment, size - 2, size)
-            a = (a * modinv) % size
-            b = (b * modinv) % size
+            modinv = pow(increment, deck_size - 2, deck_size)
+            a, b = (a * modinv) % deck_size, (b * modinv) % deck_size
+
+        else:
+            raise Exception(f'Unknown move "{move}"')
 
     return a, b
+
+
+def polypow(a, b, power, modulo):
+    if power == 0:
+        return 1, 0
+
+    if power & 1 == 0:
+        return polypow((a * a) % modulo, ((a + 1) * b) % modulo, power >> 1, modulo)
+
+    c, d = polypow(a, b, power - 1, modulo)
+    return (a * c) % modulo, (a * d + b) % modulo
 
 
 def main():
     moves = [line.strip() for line in sys.stdin]
 
-    a, b = shuffle(moves, 10007)
-    print([(a * i + b) % 10007 for i in range(10007)].index(2019))
+    deck_size = 10007
+    a, b = get_coefficients(moves, deck_size)
+    deck = [(a * i + b) % deck_size for i in range(deck_size)]
+    print(deck.index(2019))
 
-    # a, b = shuffle(moves, 119315717514047)
-    # print(a, b)
-
-    # a = (a * 101741582076661) % 119315717514047
-    # b = (b * 101741582076661) % 119315717514047
-    # print(a, b)
-
-    # 2344101622014 - too low
-    # print((a * 101741582076661* 2020 + b * 101741582076661) % 119315717514047)
+    deck_size = 119315717514047
+    a, b = get_coefficients(moves, deck_size)
+    a, b = polypow(a, b, power=101741582076661, modulo=deck_size)
+    print((a * 2020 + b) % deck_size)
 
 
 if __name__ == '__main__':
