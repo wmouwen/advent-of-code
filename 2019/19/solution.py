7e3daa30574dae3e962127b17ddb718157e27172
Willem Mouwen <willem@mouwen.dev>
2024-12-29 20:37:32 +0100

    2019 day 19 part 2

--- 2019/19/solution.py
+++ 2019/19/solution.py
@@ -5,25 +5,43 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../i
 from intcode import IntcodeComputer
 
 
+def is_within_beam(program, x, y):
+    coordinates = [x, y]
+    output = []
+
+    computer = IntcodeComputer(
+        program=program,
+        input_callback=lambda: coordinates.pop(0),
+        output_callback=lambda value: output.append(value)
+    )
+    computer.run()
+
+    return output[0] == 1
+
+
 def main():
     program = list(map(int, sys.stdin.readline().strip().split(',')))
 
-    beam_size = 0
+    print(sum(
+        1
+        for ay in range(50)
+        for ax in range(50)
+        if is_within_beam(program, ax, ay)
+    ))
 
-    for y in range(50):
-        for x in range(50):
-            coordinates = [x, y]
-            output = []
+    x, y = 0, 99
 
-            computer = IntcodeComputer(
-                program=program,
-                input_callback=lambda: coordinates.pop(0),
-                output_callback=lambda value: output.append(value)
-            )
-            computer.run()
-            beam_size += int(output[0] == 1)
+    while True:
+        if not is_within_beam(program, x, y):
+            x += 1
+            continue
 
-    print(beam_size)
+        if y - 99 >= 0 and is_within_beam(program, x + 99, y - 99):
+            break
+
+        y += 1
+
+    print(x * 10000 + (y - 99))
 
 
 if __name__ == '__main__':
