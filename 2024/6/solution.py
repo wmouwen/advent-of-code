a0dcccd6ee75d27263a7969cc0d645eb1a96e8b6
Willem Mouwen <willem@mouwen.dev>
2025-08-17 17:07:09 +0200

    2024 day 6 performance

--- 2024/6/solution.py
+++ 2024/6/solution.py
@@ -1,81 +1,89 @@
 import sys
-from collections import namedtuple
-
-Vector = namedtuple('Vector', 'y x')
-
-
-class LoopException(Exception):
-    pass
+from functools import cache
+from operator import itemgetter
+from typing import NamedTuple
 
 
-def steps_to_exit(grid: list[list[str]]) -> set[Vector]:
-    dirs = [Vector(y=-1, x=0), Vector(y=0, x=1), Vector(y=1, x=0), Vector(y=0, x=-1)]
-    guard_start = find_guard_start(grid)
-    curr_pos = guard_start
-    curr_dir = 0
-    visited_positions = {curr_pos}
-    patrol_route = {(curr_pos, curr_dir)}
+class Vector(NamedTuple):
+    x: int
+    y: int
+
+    @cache
+    def __add__(self, other: 'Vector') -> 'Vector':
+        return Vector(x=self.x + other.x, y=self.y + other.y)
+
+
+DIRECTIONS = (
+    Vector(x=0, y=-1),
+    Vector(x=1, y=0),
+    Vector(x=0, y=1),
+    Vector(x=-1, y=0),
+)
+
+
+def read_input() -> tuple[Vector, set[Vector], Vector]:
+    grid = [line.strip() for line in sys.stdin if line.strip()]
+
+    grid_size = Vector(x=len(grid[0]), y=len(grid))
+    walls = set(
+        Vector(x=x, y=y)
+        for y, row in enumerate(grid)
+        for x, cell in enumerate(row)
+        if cell == '#'
+    )
+    start = next(
+        Vector(x=x, y=y)
+        for y, row in enumerate(grid)
+        for x, cell in enumerate(row)
+        if cell == '^'
+    )
+
+    return grid_size, walls, start
+
+
+def path_to_escape(
+    grid_size: Vector,
+    walls: set[Vector],
+    start: tuple[Vector, int],
+) -> set[tuple[Vector, int]] | None:
+    path = {start}
+    position, orientation = start
 
     while True:
-        next_pos = Vector(
-            y=curr_pos.y + dirs[curr_dir].y, x=curr_pos.x + dirs[curr_dir].x
-        )
-        while 0 <= next_pos.y < len(grid) and 0 <= next_pos.x < len(grid[next_pos.y]):
-            if grid[next_pos.y][next_pos.x] != '#':
-                break
-            curr_dir = (curr_dir + 1) % len(dirs)
-            next_pos = Vector(
-                y=curr_pos.y + dirs[curr_dir].y, x=curr_pos.x + dirs[curr_dir].x
-            )
+        next_pos = position + DIRECTIONS[orientation]
 
-        if not (
-            0 <= next_pos.y < len(grid) and 0 <= next_pos.x < len(grid[next_pos.y])
-        ):
+        while next_pos in walls:
+            orientation = (orientation + 1) % len(DIRECTIONS)
+            next_pos = position + DIRECTIONS[orientation]
+
+        if not (0 <= next_pos.y < grid_size.y and 0 <= next_pos.x < grid_size.x):
             break
 
-        curr_pos = next_pos
+        position = next_pos
 
-        if (curr_pos, curr_dir) in patrol_route:
-            raise LoopException
+        key = (position, orientation)
+        if key in path:
+            return None
 
-        patrol_route.add((curr_pos, curr_dir))
-        visited_positions.add(curr_pos)
+        path.add(key)
 
-    return visited_positions
-
-
-def find_guard_start(grid) -> Vector:
-    for y, row in enumerate(grid):
-        for x, cell in enumerate(row):
-            if cell == '^':
-                return Vector(y=y, x=x)
-
-    raise Exception('No guard found')
+    return path
 
 
 def main():
-    grid: list[list[str]] = []
+    grid_size, walls, start = read_input()
 
-    for line in sys.stdin:
-        if line.strip() == '':
-            break
+    path = path_to_escape(grid_size, walls, (start, 0))
+    positions = set(map(itemgetter(0), path))
+    print(len(positions))
 
-        grid.append(list(line.strip()))
-
-    visited_positions = steps_to_exit(grid)
-    print(len(visited_positions))
-
-    visited_positions.remove(find_guard_start(grid))
-    obstacles_causing_loops = 0
-    for position in visited_positions:
-        grid[position.y][position.x] = '#'
-        try:
-            steps_to_exit(grid)
-        except LoopException:
-            obstacles_causing_loops += 1
-        grid[position.y][position.x] = '.'
-
-    print(obstacles_causing_loops)
+    loop_options = [
+        position
+        for position in positions
+        if position != start
+        and path_to_escape(grid_size, walls | {position}, (start, 0)) is None
+    ]
+    print(len(loop_options))
 
 
 if __name__ == '__main__':
