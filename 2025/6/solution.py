2e5abb514935c33e3f4acbc984fc12d5e6cd9fc7
Willem Mouwen <willem@mouwen.dev>
2025-12-06 15:22:05 +0100

    2025 day 6 part 2

--- 2025/6/solution.py
+++ 2025/6/solution.py
@@ -4,7 +4,7 @@ import sys
 
 
 def solve_normal(lines: list[str]) -> int:
-    numbers, operations = [], []
+    numbers, operations, total = [], [], 0
 
     for line in lines:
         if matches := re.findall(r'\d+', line):
@@ -12,23 +12,41 @@ def solve_normal(lines: list[str]) -> int:
         if matches := re.findall(r'[+*]', line):
             operations = matches
 
-    total = 0
     for x, op in enumerate(operations):
-        match op:
-            case '+':
-                total += sum(numbers[y][x] for y in range(len(numbers)))
-            case '*':
-                total += math.prod(numbers[y][x] for y in range(len(numbers)))
+        if op == '+':
+            total += sum(numbers[y][x] for y in range(len(numbers)))
+        if op == '*':
+            total += math.prod(numbers[y][x] for y in range(len(numbers)))
 
     return total
 
 
 def solve_rtl(lines: list[str]) -> int:
-    return 0
+    numbers, total = [], 0
+
+    for x in range(max(len(lines[y]) for y in range(len(lines))) - 1, -1, -1):
+        number = ''.join(
+            lines[y][x] if x < len(lines[y]) else ' ' for y in range(len(lines) - 1)
+        ).strip()
+
+        if number == '':
+            continue
+
+        numbers.append(int(number))
+
+        op = lines[-1][x] if x < len(lines[-1]) else ' '
+        if op == '+':
+            total += sum(numbers)
+            numbers.clear()
+        if op == '*':
+            total += math.prod(numbers)
+            numbers.clear()
+
+    return total
 
 
 def main():
-    lines = [line.strip() for line in sys.stdin]
+    lines = [line.rstrip() for line in sys.stdin]
 
     print(solve_normal(lines))
     print(solve_rtl(lines))
